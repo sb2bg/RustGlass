e84193eee0a890c58e917e6c55db7b82b5dfb205
sb2bg <sullivanbognar@gmail.com>
2021-07-11 18:37:42 -0500

    Put nothing if no position instead of unknown location

--- src/errorsystem/mod.rs
+++ src/errorsystem/mod.rs
@@ -1,22 +1,22 @@
 use std::borrow::Cow;
 
 use crate::errorsystem::error_type::{error_to_string, ErrorType};
-use crate::lang::lexer::position::Position;
 use crate::lang::lexer::position::fragment::PositionFragment;
+use crate::lang::lexer::position::Position;
 
 pub mod error_type;
 
 const RED: &str = "\x1b[31m";
 
-pub fn dispatch_error(error: ErrorType, args: Option<&[&str]>, pos: Option<PositionFragment>) {
-    eprintln!("{}Fatal exception during runtime -> \"{}\" at {}", RED, format(error_to_string(error), args), match pos {
-        Some(unwrapped) => unwrapped.to_string(),
-        None => String::from("unknown location")
+pub fn dispatch_error(error: ErrorType, args: Option<&[String]>, pos: Option<PositionFragment>) {
+    eprintln!("{}Fatal exception during runtime -> \"{}\"{}", RED, format(error_to_string(error), args), match pos {
+        Some(unwrapped) => format_args!(" at {}", unwrapped.to_string()).to_string(),
+        None => String::new()
     });
     std::process::exit(0);
 }
 
-fn format(msg: &'static str, args: Option<&[&str]>) -> String {
+fn format(msg: &'static str, args: Option<&[String]>) -> String {
     return match args {
         Some(args) => {
             let mut temp = Cow::from(msg);
