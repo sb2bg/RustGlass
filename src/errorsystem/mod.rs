11cf13b28b367251b2a8075148c186dc779abf67
sb2bg <sullivanbognar@gmail.com>
2021-07-13 06:09:08 -0500

    Print red using termcolor instead of red color escape code (will revisit potentially)

--- src/errorsystem/mod.rs
+++ src/errorsystem/mod.rs
@@ -1,22 +1,29 @@
 use std::borrow::Cow;
+use std::io::Write;
 
-use crate::errorsystem::error_type::{error_to_string, ErrorType};
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+
+use crate::errorsystem::error_type::ErrorType;
 use crate::lang::lexer::position::fragment::PositionFragment;
-use crate::lang::lexer::position::Position;
 
 pub mod error_type;
 
-const RED: &str = "\x1b[31m";
-
 pub fn dispatch_error(error: ErrorType, args: Option<&[String]>, pos: Option<PositionFragment>) {
-    eprintln!("{}Fatal exception during runtime -> \"{}\"{}", RED, format(error_to_string(error), args), match pos {
+    if !print_red(format_args!("\n\tFatal exception during runtime -> \"{}\"{}", format(error.to_string(), args), match pos {
         Some(unwrapped) => format_args!(" at {}", unwrapped.to_string()).to_string(),
         None => String::new()
-    });
-    std::process::exit(0);
+    }).to_string()).is_ok() {
+        eprintln!("Failed to write glass error message");
+    } else {
+        if !print_clear().is_ok() {
+            eprintln!("Failed to default console colors");
+        }
+    }
+
+    std::process::exit(1);
 }
 
-fn format(msg: &'static str, args: Option<&[String]>) -> String {
+fn format(msg: String, args: Option<&[String]>) -> String {
     return match args {
         Some(args) => {
             let mut temp = Cow::from(msg);
@@ -29,4 +36,16 @@ fn format(msg: &'static str, args: Option<&[String]>) -> String {
         }
         None => String::from(msg)
     };
+}
+
+fn print_red(msg: String) -> std::io::Result<()> {
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Red)))?;
+    return writeln!(&mut stdout, "{}", msg);
+}
+
+fn print_clear() -> std::io::Result<()> {
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+    stdout.set_color(&ColorSpec::default())?;
+    return writeln!(&mut stdout);
 }
