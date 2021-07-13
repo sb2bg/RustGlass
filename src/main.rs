131b3ede313b3bbdc8d309924a0d5a75b229939e
sb2bg <sullivanbognar@gmail.com>
2021-07-13 06:08:26 -0500

    Finish cli

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use std::fs;
+
 use clap::{App, Arg};
+
+use crate::errorsystem::dispatch_error;
+use crate::errorsystem::error_type::ErrorType;
+use crate::lang::lexer::Lexer;
 
 mod errorsystem;
 mod lang;
@@ -8,12 +14,34 @@ fn main() {
         .version("1.0.0")
         .author("Sullivan B")
         .about("Dynamically typed language written in RustLang")
-        .arg(Arg::with_name("run")
+        .arg(Arg::with_name("filename")
             .index(1)
-            .takes_value(true))
+            .takes_value(true)
+            .required(true))
         .get_matches();
 
-    if let Some(i) = matches.value_of("run") {
-        println!("{}", i);
+    // todo - pull filename from clap
+    let filename = matches.value_of("filename").unwrap().to_string();
+
+    let src = match read_file(String::from(filename.clone())) {
+        Ok(res) => res,
+        Err(_) => {
+            dispatch_error(ErrorType::UnknownFile, Some(&[String::from(filename)]), None);
+            panic!(); // (not called) avoid incompatible arm type error
+        }
+    };
+
+    let mut lexer = Lexer::new(filename, src);
+    let tokens = lexer.lex();
+
+    for token in tokens {
+        println!("{}", token.to_string())
     }
+}
+
+fn read_file(file: String) -> Result<String, std::io::Error> {
+    return match fs::read_to_string(file) {
+        Ok(contents) => Ok(contents),
+        Err(err) => Err(err)
+    };
 }
