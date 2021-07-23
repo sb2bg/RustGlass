09817785b086d929c85df6d4a682db2ed1c93717
sb2bg <sullivanbognar@gmail.com>
2021-07-23 16:17:57 -0500

    Replace PositionFragment with Position

--- src/lang/lexer/token/mod.rs
+++ src/lang/lexer/token/mod.rs
@@ -1,24 +1,24 @@
-use crate::lang::lexer::position::fragment::PositionFragment;
+use crate::lang::lexer::position::Position;
 use crate::lang::lexer::token::token_type::TokenType;
 
 pub mod token_type;
 
-pub struct Token {
+pub struct Token<'a> {
     token_type: TokenType,
     value: String,
-    pos: PositionFragment,
+    pos: Position<'a>,
 }
 
-impl Token {
-    pub fn new(token_type: TokenType, value: Option<String>, pos: PositionFragment) -> Self {
-        return Token {
+impl<'a> Token<'a> {
+    pub fn new(token_type: TokenType, value: Option<String>, pos: Position<'a>) -> Self {
+        Token {
             token_type,
             value: match value {
                 Some(value) => value,
                 None => String::new()
             },
             pos,
-        };
+        }
     }
 
     pub fn get_type(&self) -> TokenType {
@@ -26,8 +26,8 @@ impl Token {
     }
 }
 
-impl ToString for Token {
+impl ToString for Token<'_> {
     fn to_string(&self) -> String {
-        return format_args!("{}{}", self.token_type.to_string(), if self.value.is_empty() { String::new() } else { format_args!("({})", self.value).to_string() }).to_string();
+        format!("{}{}", self.token_type.to_string(), if self.value.is_empty() { String::new() } else { format_args!("({})", self.value).to_string() })
     }
 }
