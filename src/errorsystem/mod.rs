4d5675cc3d0cea2af94b530c2cac11bb2e3db6cd
sb2bg <sullivanbognar@gmail.com>
2021-07-24 12:25:27 -0500

    Tidy error message

--- src/errorsystem/mod.rs
+++ src/errorsystem/mod.rs
@@ -4,7 +4,7 @@ use crate::lang::lexer::position::Position;
 pub mod error_type;
 
 pub fn dispatch_error(error: ErrorType, pos: Option<Position>) {
-    eprintln!("\n\tFatal exception during runtime -> \"{}\"{}", error.to_string(), match pos {
+    eprintln!("\n\tFatal exception during runtime -> \"{}{}\"", error.to_string(), match pos {
         Some(unwrapped) => format!(" at {}", unwrapped.to_string()),
         None => String::new()
     });
