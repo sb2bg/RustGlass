6a26651fa11746001ebac97bf25abcd2b4154be5
sb2bg <sullivanbognar@gmail.com>
2022-02-14 23:18:57 -0600

    Skeleton

--- src/lang/parser/mod.rs
+++ src/lang/parser/mod.rs
@@ -1,6 +1,7 @@
 use crate::errorsystem::dispatch_error;
 use crate::errorsystem::error_type::ErrorType;
 use crate::lang::lexer::token::Token;
+use crate::Lexer;
 
 pub struct Parser<'a> {
     tokens: Vec<Token<'a>>,
@@ -8,13 +9,4 @@ pub struct Parser<'a> {
     current: &'a Token<'a>,
 }
 
-impl<'a> Parser<'a> {
-}
-
-impl Iterator for Parser<'_> {
-    type Item = ();
-
-    fn next(&mut self) -> Option<Self::Item> {
-        todo!()
-    }
-}
+impl<'a> Parser<'a> {}
