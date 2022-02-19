8b9ad8ccd2b89165e7d276c5a46bb83349ce5ef1
sb2bg <sullivanbognar@gmail.com>
2022-02-18 22:03:41 -0600

    Remove whitespace from beginning of line in errors

--- src/lang/lexer/position/mod.rs
+++ src/lang/lexer/position/mod.rs
@@ -25,27 +25,30 @@ impl<'a> Position<'a> {
         self
     }
 
-    fn get_line(&self, line: usize) -> &str {
-        let result = match self.source.lines().nth(line) {
+    fn get_line(&self, line: usize) -> (&str, usize) {
+        let orig = match self.source.lines().nth(line) {
             Some(line) => line,
             None => {
                 dispatch_error!(ErrorType::GenericError("converting position to string"));
             }
         };
 
-        result
+        let result = orig.trim_start();
+        
+        (result, orig.len() - result.len())
     }
 }
 
 impl Display for Position<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let line = self.row - 1;
-        let col = self.column - 1;
+        let (str_line, offset) = self.get_line(line);
+        let col = self.column - 1 - offset;
 
         write!(
             f,
             "\n\n\t\t{}\n\t\t{}^\n\t[{}(Ln:{} Col:{})]",
-            self.get_line(line),
+            str_line,
             if col > 0 { " ".repeat(col - 1) } else { String::new() },
             self.filename, self.row, self.column
         )
