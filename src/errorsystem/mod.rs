00374e887b2cfe584435aa6eadec1a2d35ae9fb1
sb2bg <sullivanbognar@gmail.com>
2022-03-27 19:56:39 -0500

    Add an ErrorHandler struct to handle try catch blocks (for interpreter)

--- src/errorsystem/mod.rs
+++ src/errorsystem/mod.rs
@@ -16,3 +16,22 @@ macro_rules! dispatch_error {
     };
 }
 
+pub struct ErrorHandler {
+    in_catch_block: bool,
+}
+
+impl ErrorHandler {
+    pub fn new() -> Self {
+        Self { in_catch_block: false }
+    }
+
+    // if the error occurs in a catch block, we don't want to print the error message, but rather
+    // give the error to the catch block and let it handle it
+    pub fn handle_runtime_error<'a>(&mut self, error: ErrorType<'a>, pos: Position) -> ErrorType<'a> {
+        if self.in_catch_block {
+            error
+        } else {
+            dispatch_error!(error, pos);
+        }
+    }
+}
