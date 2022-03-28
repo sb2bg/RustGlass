e12451fd1fb5362940b769af12ae918645820855
sb2bg <sullivanbognar@gmail.com>
2022-03-27 19:59:17 -0500

    add node modules and add Display inheritance to Node trait

--- src/lang/parser/node/mod.rs
+++ src/lang/parser/node/mod.rs
@@ -1,13 +1,16 @@
-use std::any::Any;
+use std::fmt::Display;
 
 use crate::lang::lexer::token::Token;
 
-pub trait Node {
+pub mod bin_op_node;
+pub mod unary_node;
+pub mod string_node;
+pub mod number_node;
+pub mod bool_node;
+pub mod void_node;
+
+// todo: add macro to auto generate visitor methods and get_token methods for all nodes (and also probably display methods)
+pub trait Node: Display {
     fn get_token(&self) -> &Token;
     fn visit(&self, visitor: ());
-}
-
-pub struct BinOpNode<'a> {
-    pub token: &'a Token<'a>,
-    pub value: (&'a dyn Node, &'a dyn Node),
 }
